e5ce8dd83e81e1203cd555bf73c4d9d5ee4562cb
João Lucas G. Varotti <jlgeno@varotti.tech>
2022-09-26 18:14:55 +0200

    Initial commit

---
+++ kettle.c
@@ -0,0 +1,31 @@
+#include "pico/stdlib.h"
+
+#define Temp1 26
+#define Temp2 27
+#define Temp3 28
+
+#define I2C0_SDA 4
+#define I2C0_SCL 5
+
+#define EncoderA 16
+#define EncoderB 17
+#define BotaoEncoder 18
+
+#define BotaoFerve 20
+#define BotaoCafe 21
+#define BotaoCha 14
+
+#define LED_Placa 25
+#define LED_Pronto 19
+#define LED_Heating 15
+
+#define Buzzer 22
+
+void EnviaChar(char);
+void EnviaFrase(char[40]);
+
+int main()
+{
+
+}
+
